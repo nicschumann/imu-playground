fac619be96ae74e8d288ea518879a6a4e74c7e7b
nicschumann <nic@nicschumann.co>
2022-10-12 13:22:12 -0400

    [server] finalizes server

--- server/main.py
+++ server/main.py
@@ -1,14 +1,15 @@
 import time
 from multiprocessing import Process, Queue
-from queue import LifoQueue
-# import busio
-# import board
-# import digitalio
-# from adafruit_bno055 import BNO055_I2C
+
+import busio
+import board
+import digitalio
+from adafruit_bno055 import BNO055_I2C
 
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 QUEUE_MAX_SIZE=1
 
@@ -16,15 +17,31 @@ thread = None
 app = FastAPI()
 data = Queue(maxsize=QUEUE_MAX_SIZE)
 
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = BNO055_I2C(i2c)
 
 def thread_main(thread_id):
     try:
         while True:
-            t = time.time()
-            data.put({'time': t, 'id': thread_id})
+            x, y, z = sensor.euler
+            if x is not None and y is not None and z is not None:
+                timestamp = time.time()
+                data.put(
+                    {'t': timestamp, 'x': x, 'y': y, 'z': z}
+                )
 
     except KeyboardInterrupt:
         return
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 async def start_background_processes():
@@ -34,23 +51,19 @@ async def start_background_processes():
     thread.start()
 
 @app.on_event("shutdown")
-async def start_background_processes():
+async def stop_background_processes():
     global thread
 
     if thread is not None:
         thread.terminate()
 
 
-@app.on_event("shutdown")
-async def stop_background_processes():
-    global thread
-    thread.terminate()
-
 
 @app.get('/imu')
 def get_imu_data():
     element = data.get()
-    return element
+    
+    return element # "{'time': t, 'id': thread_id}"
 
 
 if __name__ == '__main__':
